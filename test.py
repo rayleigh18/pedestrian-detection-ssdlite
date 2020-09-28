a0185fd012f2d221773e5a89506d4b9ebf723dd8
arifbintanga <arifbintang354@gmail.com>
2020-09-28 23:22:13 +0700

    Adding Image Files and Iteration in test.py
    Iterating new images in image folder

--- test.py
+++ test.py
@@ -1,13 +1,20 @@
 import cv2
+from imutils import paths
+import imutils
 from pedestrian_detection_ssdlite import api
 from matplotlib import pyplot as plt
 
-img = cv2.imread('test_img/example.jpg')
-bbox_list = api.get_person_bbox(img, thr=0.6)
-print(bbox_list)
+imagePaths = list(paths.list_images("test_img"))
+    
+for imagePath in imagePaths:
+    img = cv2.imread(imagePath)
+    img = imutils.resize(img, width=min(400, img.shape[1]))
+    
+    bbox_list = api.get_person_bbox(img, thr=0.6)
+    print(bbox_list)
 
-for i in bbox_list:
-    cv2.rectangle(img, i[0], i[1], (125, 255, 51), thickness=2)
+    for i in bbox_list:
+        cv2.rectangle(img, i[0], i[1], (125, 255, 51), thickness=2)
 
-plt.imshow(img[:, :, ::-1])
-plt.show()
+    plt.imshow(img[:, :, ::-1])
+    plt.show()
