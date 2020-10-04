f55f6c0d082f34a3c3c7919b5af034fbfa4a43ec
rayleigh18 <sitanggangirfan271000@gmail.com>
2020-10-05 00:02:24 +0700

    Ngebuat udah bisa ama video

--- test.py
+++ test.py
@@ -4,17 +4,36 @@ import imutils
 from pedestrian_detection_ssdlite import api
 from matplotlib import pyplot as plt
 
-imagePaths = list(paths.list_images("test_img"))
-    
-for imagePath in imagePaths:
-    img = cv2.imread(imagePath)
+cap = cv2.VideoCapture(0)
+
+while(1):
+    berhasil, img = cap.read()
     img = imutils.resize(img, width=min(400, img.shape[1]))
-    
+
     bbox_list = api.get_person_bbox(img, thr=0.6)
     print(bbox_list)
 
     for i in bbox_list:
         cv2.rectangle(img, i[0], i[1], (125, 255, 51), thickness=2)
+    cv2.imshow('tracking', img)
 
-    plt.imshow(img[:, :, ::-1])
-    plt.show()
+    k = cv2.waitKey(5) & 0xFF
+    if k == 27:
+        cap.release()
+        cv2.destroyAllWindows()
+        break
+
+# imagePaths = list(paths.list_images("test_img"))
+    
+# for imagePath in imagePaths:
+#     img = cv2.imread(imagePath)
+#     img = imutils.resize(img, width=min(400, img.shape[1]))
+    
+#     bbox_list = api.get_person_bbox(img, thr=0.6)
+#     print(bbox_list)
+
+#     for i in bbox_list:
+#         cv2.rectangle(img, i[0], i[1], (125, 255, 51), thickness=2)
+
+#     plt.imshow(img[:, :, ::-1])
+#     plt.show()
